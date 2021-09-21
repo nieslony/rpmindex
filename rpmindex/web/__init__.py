c5f0b881bd8bb004d7c3624e54c71d0e8fa9eb52
Claas Nieslony <bb@nieslony.at>
2021-09-21 23:45:46 +0200

    Read directiries and display RPM summary

--- rpmindex/web/__init__.py
+++ rpmindex/web/__init__.py
@@ -1,9 +1,41 @@
 import flask
+import logging
+import os
+import yaml
 
 def create_app():
     app = flask.Flask(__name__)
+    app.logger.setLevel(logging.DEBUG)
 
+    app.logger.info("Loading blueprints")
     import rpmindex.web.index
     app.register_blueprint(rpmindex.web.index.bp)
+
+    @app.before_first_request
+    def bfr():
+        default_config_fn = f"{os.path.dirname(__file__)}/../../rpmindex.yml"
+        app.logger.info(f"Loading default config from {default_config_fn}")
+        with open(default_config_fn) as f:
+            config = yaml.load(f, Loader=yaml.Loader)
+        # app.logger.debug(config)
+
+        custom_config_fn = flask.request.environ.get("CONFIG_PATH")
+        if not custom_config_fn:
+            custom_config_fn = os.environ.get("CONFIG_PATH")
+        if custom_config_fn:
+            app.logger.info(f"Loading custom config from {custom_config_fn}")
+            try:
+                with open(custom_config_fn) as f:
+                    custom_config = yaml.load(f, Loader=yaml.FullLoader)
+            except Exception as ex:
+                app.logger.fatal(f"Cannot load custom config: {str(ex)}")
+                return
+            dict_merge(config, custom_config)
+        else:
+            app.logger.info("No custom config given, skipping.")
+        app.config.update(**config)
+        # app.logger.debug(config)
+
+        app.logger.info("After startup initialization finished.")
 
     return app
--- rpmindex/web/index.py
+++ rpmindex/web/index.py
@@ -1,10 +1,67 @@
 import flask
+import os
+import datetime
+import rpm
 
 bp = flask.Blueprint("index", __name__)
 
-@bp.route("/")
-def index():
+@bp.route('/', defaults={'path': ''})
+@bp.route('/<path:path>')
+def index(path):
+    app = flask.current_app
+    repo_path = os.path.realpath(app.config["repo"]["path"])
+    repo_name =  app.config["repo"]["name"]
+    folder = os.path.realpath(f"{repo_path}/{path}")
+
+    app.logger.info(f"Reading folder {folder}")
+    files = []
+    dirs = []
+    if not path in ["/", ""]:
+        dirs.append({
+            "name": "..",
+            "size": "",
+            "modified": ""
+            })
+    try:
+        for filename in os.listdir(folder):
+            filepath = f"{folder}/{filename}"
+            stat = os.stat(filepath)
+            mod_time = datetime.datetime.fromtimestamp(stat.st_mtime)
+            if os.path.isfile(filepath):
+                if filename.endswith(".rpm"):
+                    app.logger.info(f"{filename} is RPM")
+                    fd = os.open(filepath, os.O_RDONLY)
+                    ts = rpm.ts()
+                    try:
+                        headers = ts.hdrFromFdno(fd)
+                        description = headers[rpm.RPMTAG_SUMMARY]
+                    except Exception as ex:
+                        app.logger.error(f"Error getting headers from {filepath} :{str(ex)}")
+                        description = ""
+                    os.close(fd)
+                else:
+                    app.logger.info(f"{filename} is not RPM")
+                    description = ""
+
+                files.append({
+                    "name": filename,
+                    "size": stat.st_size,
+                    "modified": mod_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    "description": description
+                    })
+            elif os.path.isdir(filepath):
+                dirs.append({
+                    "name": filename,
+                    "size": stat.st_size,
+                    "modified": mod_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    })
+    except FileNotFoundError as ex:
+        flask.abort(404)
+
     args = {
-        "title": "The title"
-            }
+        "title": repo_name,
+        "path": path,
+        "files": files,
+        "dirs": dirs
+        }
     return flask.render_template("index.html", **args)
